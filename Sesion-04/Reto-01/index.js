c010fcef41506d6b94d51fa49d4d0b4cbb836b1a
Ricardo Solano Fadul <risolfa@gmail.com>
2022-02-02 21:51:22 -0600

    reto 1 lo puse mas elegante

--- Sesion-04/Reto-01/index.js
+++ Sesion-04/Reto-01/index.js
@@ -2,9 +2,7 @@ nums = [2, 6, 8, 14, 4, 2, 18, 0];
 
 function avg(arr) {
     let sum = 0;
-    arr.forEach(element => {
-        sum += element;
-    });
+    arr.forEach(element => sum += element);
     return sum / arr.length;
 }
 console.log(avg(nums));
