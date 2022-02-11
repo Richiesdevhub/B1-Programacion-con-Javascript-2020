3e79602e5101bd359a3263f9882d2e44d7ba4482
Ricardo Solano Fadul <risolfa@gmail.com>
2022-02-10 21:59:47 -0600

    termine ejercicio reto 3

---
+++ Sesion-05/Reto-03/reto3.js
@@ -0,0 +1,10 @@
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+}
